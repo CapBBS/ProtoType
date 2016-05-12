ae171beecdcf11a6257d1242153bb9b45bdb5d09
DongHyuk <djEjgrpgkwl@gmail.com>
2016-05-12 16:25:01 +0900

    add Broadcast Receiver

--- app/src/main/java/com/example/administrator/test1/MainActivity.java
+++ app/src/main/java/com/example/administrator/test1/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.wifi.WpsInfo;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
@@ -27,7 +28,7 @@ public class MainActivity extends Activity{
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_Main);
+        setContentView(R.layout.activity_main);
 
         btnFindpeer = (Button)findViewById(R.id.btnFindpeer);
         btnConnect = (Button)findViewById(R.id.btnConnect);
@@ -41,12 +42,15 @@ public class MainActivity extends Activity{
         manager = (WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
         channel = manager.initialize(this, getMainLooper(), null);
 
+        wifiBroadcastReceiver = new WifiBroadcastReceiver(manager, channel, this);
+
         btnFindpeer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 manager.discoverPeers(channel,null);
             }
         });
+
 
 
     }
